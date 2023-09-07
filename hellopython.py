73184531d5333ec1b8083b7fd370c0e275a505af
cuongboycodon <116739263+cuongboycodon@users.noreply.github.com>
2023-09-07 08:40:13 +0700

    Add files via upload

---
+++ hellopython.py
@@ -0,0 +1,20 @@
+import math
+print("Hello Python")
+a = 1
+print(a)
+b = 2
+
+print(math.sin(math.pi/4))    
+math.cos(math.pi)      
+math.tan(math.pi/6)    
+math.hypot(12,5)       
+math.atan(0.5773502691896257) 
+math.asin(0.7071067811865476) 
+str1 = "Hello"
+str2 = 'world'
+print(str1)
+print(str2)
+str = str1 + " " + str2
+print(str)
+count = len("Hello world")
+print(count)
